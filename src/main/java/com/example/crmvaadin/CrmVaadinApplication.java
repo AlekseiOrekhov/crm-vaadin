1f44efeb797d7aeb6c6a53b84234d12ba89714fd
orekhov_as <orekhov_as@magnit.ru>
2023-01-19 13:58:15 +0300

    first commit

---
+++ src/main/java/com/example/crmvaadin/CrmVaadinApplication.java
@@ -0,0 +1,13 @@
+package com.example.crmvaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmVaadinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crmvaadin/CrmVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crmvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmVaadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
